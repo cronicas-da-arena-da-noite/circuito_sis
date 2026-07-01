15e978ec25e7b872d0f6deeb6bdce64b3f55d664
cronicas-da-arena-da-noite <estudosconta77@gmail.com>
2026-07-01 12:42:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function denunciaRapida(){
     );
 
 }
+
+const botaoIA = document.getElementById("botao_ia");
+
+botaoIA.addEventListener("click", () => {
+    window.location.href = "./paginas/usuarios/denuncia_ia/denuncia_ia.html";
+});
